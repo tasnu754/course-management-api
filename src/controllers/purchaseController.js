3b482f3da69630a9aad6ec88a410ba4625cd0fba
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-09-14 11:14:42 +0600

    Get specific user's purchase courses using userId and fetch data from the database

--- src/controllers/purchaseController.js
+++ src/controllers/purchaseController.js
@@ -42,4 +42,22 @@ const purchaseCourse = async (req, res) => {
   }
 };
 
-export default { purchaseCourse };
+const getUserPurchases = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const purchases = await Purchase.find({ userId })
+      .populate("courseId", "title description instructor price")
+      .sort({ purchaseDate: -1 });
+
+    res.json({
+      success: true,
+      data: purchases,
+      count: purchases.length,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export default { purchaseCourse, getUserPurchases };
